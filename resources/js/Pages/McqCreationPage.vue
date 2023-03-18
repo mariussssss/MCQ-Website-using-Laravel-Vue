869ae0779e5c9e31daecbf92f829d2979335a4f4
mariussssss <80851564+mariussssss@users.noreply.github.com>
2023-03-18 01:09:03 +0000

    Update McqCreationPage.vue
    Update creation page.
    Still need to improve the "add question" button and to make all the form connected with the database.

--- resources/js/Pages/McqCreationPage.vue
+++ resources/js/Pages/McqCreationPage.vue
@@ -16,31 +16,28 @@ import { Link } from '@inertiajs/vue3';
                         <input v-model="mcq.title" type="text" class="ml-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your title ..." required>
                     </div>
                     <div class="pl-6 flex items-center mb-4">
-                        <label class="block font-medium text-gray-900 dark:text-white">Color : </label>
-                        <button v-for="colorClass in classButtons" class="cursor-pointer border-gray-200  ml-2 w-20 h-20 bg-gradient-to-br hover:bg-gradient-to-tl focus:ring-4 rounded-lg text-center mr-2 mb-2 peer-checked:border-blue-600 dark:peer-checked:text-gray-300 dark:focus:ring-blue-800" :class="[colorClass]">
-                        
+                        <label class="block font-medium text-gray-900 dark:text-white">Theme color : </label>
+                        <button v-for="(color, index) in classButtons" @click=" colorSelected = index" :class=" colorSelected == index ? 'border-solid border-black outline outline-2 outline-yellow-400 border-2 ' + color.class : color.class" class="cursor-pointer ml-2 w-20 h-20 bg-gradient-to-br hover:bg-gradient-to-tl rounded-lg mr-2 mb-2" >
+                            
                         </button>
-
-
                     </div>
-                    <div class="pl-6 flex items-center mb-4">
-                        <div>
-                            <label class="block font-medium text-gray-900 dark:text-white">Add a question : </label>
-                            <label class="block font-medium text-gray-900 dark:text-gray-500 text-xs">(you can have up to 20 questions in total)</label>
-                        </div>
-                        <input type="text" v-model="numberOfQuestion" class="ml-2 sm:text-xs w-10 h-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                        <button @click="addQuestions" class="ml-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add</button>
-                    </div>
+                    <!-- <div class="pl-6 flex items-center mb-4">
+                        <label class="block font-medium text-gray-900 dark:text-white">Preview : </label>
+                        <button :class="[colorClassChoosen]" class="text-2xl px-5 py-10 w-40 h-90 text-center cursor-pointer border-gray-200 peer-checked:border-blue-600 ml-2 bg-gradient-to-br hover:bg-gradient-to-tl focus:ring-4 rounded-lg text-center mr-2 mb-2 peer-checked:border-blue-600">
+                            {{ mcq.title }}
+                        </button>
+                    </div> -->
+                    
                     <form>
                         <div v-for="(question,index) in questionTab" class=" p-6 pt-3 mb-4 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                             <div class="mb-6">
                                 <div class="flex justify-between">
                                     <label class="h-8 mt-4 block mb-2 font-medium text-gray-900 dark:text-white">Question n°{{ index+1 }}</label>
                                     <button type="button" @click="questionTab.splice(index, 1);" class="text-red-500 text-lg mr-2 mb-4">
-                                        <font-awesome-icon icon="fa-solid fa-trash-can" />
+                                        <font-awesome-icon v-if="index!=0" icon="fa-solid fa-trash-can" />
                                     </button>
                                 </div>
-                                <input v-model="question.text" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your first question ..." required>
+                                <input v-model="question.text" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your question ..." required>
                             </div>
                             <div class="mb-6">
                                 <div class="flex items-center mb-2">
@@ -58,6 +55,14 @@ import { Link } from '@inertiajs/vue3';
                                 </div>
                             </div>
                         </div>
+                        <div class="pl-6 flex items-center mb-4">
+                            <div>
+                                <label class="block font-medium text-gray-900 dark:text-white">Add a question : </label>
+                                <label class="block font-medium text-gray-900 dark:text-gray-500 text-xs">(you can have up to 20 questions in total)</label>
+                            </div>
+                            <input type="text" v-model="numberOfQuestion" class="ml-2 sm:text-xs w-10 h-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                            <button @click="addQuestions" :disabled="questionTab.length >= 20" type="button" class="ml-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add</button>
+                        </div>
                         <button type="submit" class=" text-white from-purple-600 to-blue-500 focus:ring-blue-300 dark:focus:ring-blue-800 bg-gradient-to-br hover:bg-gradient-to-bl focus:ring-4 focus:outline-none rounded-lg px-5 py-2.5 text-center mr-2 mb-2 font-medium">Submit</button>
                     </form>
                 </div>
@@ -70,26 +75,29 @@ import { Link } from '@inertiajs/vue3';
 export default {
     data() {
         const classButtons = [
-            "text-white from-purple-600 to-blue-500",
-            "text-white from-cyan-500 to-blue-500",
-            "text-white from-green-400 to-blue-600",
-            "text-white from-purple-500 to-pink-500",
-            "text-white from-pink-500 to-orange-400 ",
-            "text-gray-900 from-teal-200 to-lime-200",
-            "text-gray-900 from-red-200 via-red-300 to-yellow-200"
+            {"class" : "from-purple-600 to-blue-500"},
+            {"class" : "from-cyan-500 to-blue-500"},
+            {"class" : "from-green-400 to-blue-600"},
+            {"class" : "from-purple-500 to-pink-500"},
+            {"class" : "from-pink-500 to-orange-400 "},
+            {"class" : "from-teal-200 to-lime-200"},
+            {"class" : "from-red-200 via-red-300 to-yellow-200"}
         ];
         let numberOfAnswer = 2;
         let numberOfQuestion = 1;
         let questionTab = [{"text" : "","numberOfAnswer" : 4, "answers" : ["", "", "", ""]}];
         let mcq = {
             "title" : ""
-        }
+        };
+        let colorSelected = -1;
+        let colorClassChoosen = ""
         return {
             numberOfAnswer,
             numberOfQuestion,
             questionTab,
             mcq,
-            classButtons
+            classButtons,
+            colorSelected
         }
     },
     methods: {
